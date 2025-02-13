1246a61f9a98e3116132ebbcdf220af12d44be8c
pancakes-proxy <zac_posey_12361_225@staffteam.learnhelp.cc>
2025-02-13 11:25:28 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const { Client } = require('ssh2');
+const RdpClient = require('node-rdpjs');
+const fs = require('fs');
 const app = express();
 
 app.use(bodyParser.json());
 
 const users = { 'user1': 'password1' };
 
-app.post('/login', (req, res) => {
-    const { username, password } = req.body;
+// Read the configuration file
+const config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+app.post('/execute', (req, res) => {
+    const { login, rdp } = config;
+
+    // Check login credentials
+    const { username, password } = login;
     if (users[username] && users[username] === password) {
-        res.status(200).send('Login successful!');
+        // Proceed with RDP if login is successful
+        const { host, rdpUsername, rdpPassword } = rdp;
+        const client = RdpClient.createClient({
+            domain: '',
+            userName: rdpUsername,
+            password: rdpPassword,
+            ip: host,
+            autoLogin: true
+        });
+
+        client.on('connect', () => {
+            res.send('Connected to Remote Desktop');
+        });
+
+        client.on('close', () => {
+            console.log('Connection closed');
+        });
+
+        client.connect();
     } else {
         res.status(401).send('Invalid username or password');
     }
-});
-
-app.post('/rsp', (req, res) => {
-    const { host, username, password, command } = req.body;
-
-    const conn = new Client();
-    conn.on('ready', () => {
-        console.log('Client :: ready');
-        conn.exec(command, (err, stream) => {
-            if (err) throw err;
-            stream.on('close', (code, signal) => {
-                console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-                conn.end();
-            }).on('data', (data) => {
-                console.log('STDOUT: ' + data);
-                res.send('STDOUT: ' + data);
-            }).stderr.on('data', (data) => {
-                console.log('STDERR: ' + data);
-                res.send('STDERR: ' + data);
-            });
-        });
-    }).connect({
-        host: host,
-        port: 22,
-        username: username,
-        password: password
-    });
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
